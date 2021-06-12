533008afd203c2854e8220316c7170fcfccc63f8
paulhorowitz <paulbhorowitz@gmail.com>
2021-06-13 08:29:30 +0930

    Add search bar and moviesrequest api

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 import { FaSearch } from 'react-icons/fa';
 
 const Header = (props) => {
   return (
-      <Container fluid className="header">
-        <Row>
-          <Col xs={6} className="align-items-center">
+      <Row className="header padding-left-md">
+          <Col xs={6}>
             <FaSearch  size={24} className="search-icon" />
+            <input name="Search" type="search" placeholder="Search" className="search-input"/>
           </Col>
           <Col>Year Filter</Col>
           <Col>Type Checkbox</Col>
-        </Row>
-    </Container>
+      </Row>
   )
 }
 
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 function MovieList(props) {
+  if (props.movies) {
   return (
     <React.Fragment>
       {props.movies.map((movie) => (
         <p>{movie.Title}</p>
       ))}
-
     </React.Fragment>
-  )
+    )
+  }
 }
 
 export default MovieList;
